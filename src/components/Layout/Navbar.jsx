f636a9455c5e3eb951710f15c2a6e852e7c2ef04
aman baghel <amanbaarish@gmail.com>
2025-01-22 02:17:32 +0530

    fifth commit

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { RiRainyLine, RiMenu3Line, RiCloseLine } from "react-icons/ri";
-import ThemeDropdown from "../shared/ThemeDropdown";
+import ThemeDropdown from "../shared/ThemeDropDown";
 import { menuItems } from "../../utils/constants";
 const Navbar = ({ currentTheme, onThemeChange }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
